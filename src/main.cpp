4329a669bea44bdbe70c0dc386c7e5e8fbf2b99e
Adrian <adrian@air-adrian-1.home>
2025-08-18 13:35:08 +0200

    Get ROM path from CLI

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
+#include "chip8.hpp"
+#include <iostream>
 
+int main(int argc, char* argv[]) {
+    
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <ROM>\n";
+        return 1;
+    }
 
-int main() {
+    const char* filename = argv[1];
+
+    Chip8 chip8;
+
+    chip8.LoadROM(filename);
 
     return 0;
 }
